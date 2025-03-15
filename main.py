18485386db26563db22c2853c2b739c5e09c9ed0
Mianmar <zawyeyaintnaing11@gmail.com>
2025-03-14 22:55:40 -0700

    Managed to add vertically waving water

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ class App:
         self.WATER_SURF = pygame.Surface((self.WIDTH, self.HEIGHT), pygame.SRCALPHA).convert_alpha()
         self.WATER_SURF.set_colorkey((0, 0, 0)) # color to refresh
         self.WATER_SURF.set_alpha(255)
+
+        # brush and stuff
+        self.grid_size = 30
+        self.pos1 = None
+        self.pos2 = None
     
     def update(self):
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                
-                if event.type == self.fps_event:
-                    pygame.display.set_caption(f"FPS: {self.clock.get_fps():.1f}")
-            
             dt = self.clock.tick(1000) / 1000
             dt *= 60
             if dt > 1:
                 dt = 1 
+            
+            self.mpos = pygame.mouse.get_pos()
             
             self.window.fill((30, 30, 30))
             self.WATER_SURF.fill((0, 0, 0))
@@ -41,10 +41,32 @@ class App:
             self.node_manager.draw(self.WATER_SURF, [0, 0])
             self.node_manager.update(dt)
 
-            pygame.draw.circle(self.window, 'red', pygame.mouse.get_pos(), 32)
-            self.window.blit(self.WATER_SURF, (0, 0), special_flags=pygame.BLEND_ADD)
+            pygame.draw.circle(self.window, 'red', self.mpos, 32)
+            self.window.blit(self.WATER_SURF, (0, 0), special_flags=pygame.BLEND_RGB_ADD)
+
+            # ui stuff
+            if self.pos1 != None:
+                self.pos2 = self.mpos[0]//self.grid_size * self.grid_size, self.mpos[1]//self.grid_size * self.grid_size, 
+                dx, dy = self.pos2[0] - self.pos1[0], self.pos2[1] - self.pos1[1]
+                pygame.draw.lines(self.window, 'white', True, [self.pos1, (self.pos1[0], self.pos1[1] + dy), self.pos2, (self.pos1[0] + dx, self.pos1[1]),])
 
             pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                
+                if event.type == self.fps_event:
+                    pygame.display.set_caption(f"FPS: {self.clock.get_fps():.1f}")
+                
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == pygame.BUTTON_LEFT:
+                        self.pos1 = (self.mpos[0]//self.grid_size * self.grid_size, self.mpos[1]//self.grid_size * self.grid_size)
+                
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == pygame.BUTTON_LEFT:
+                        '''Create Water Nodes'''
+
+                        self.pos1 = None
     
 
 if __name__ == "__main__":
--- node.py
+++ node.py
@@ -13,21 +13,28 @@ class Node:
         pygame.draw.circle(draw_surf, 'white', (self.x - camera_offset[0], self.y - camera_offset[1]), 5)
     
     def interact(self, pos):
-        if pygame.mouse.get_pressed()[0]:
-            if pygame.Rect(self.x, self.y, 16, 16).collidepoint(pos):
-                self.rect.centery = pos[1]
-                self.y = self.rect.y
-                self.vel.y = 0
-                return True
+        if self.rect.collidepoint(pos):
+            self.rect.centery = max(min(pos[1], self.ori_pos[1] + 100), self.ori_pos[1] - 100)
+            self.y = self.rect.y
+            self.vel.y = 0
+            return True
         return
 
-    def wave(self, t, amplitude=0.5, waves=5, speed=4):
+    def wave_vertical(self, t, amplitude=0.5, waves=5, speed=4):
         value = noise.pnoise1(self.x * 0.01 + t * 0.0001)
         self.vel.y += amplitude * (math.sin(waves * self.x + speed * t) + 2 * value) * self.dt
+    
+    def wave_horizontal(self, t, amplitude=0.5, waves=5, speed=4):
+        value = noise.pnoise1(self.y * 0.01 + t * 0.0001)
+        self.vel.x += amplitude * (math.sin(waves * self.y + speed * t) + 2 * value) * self.dt
 
     def update(self, delta_time):
         self.dt = delta_time
 
+        # self.apply_vertical_physics()
+        self.apply_horizontal_physics()
+
+    def apply_vertical_physics(self):
         # update movement
         extension = self.rect.y - self.ori_pos[1]
         energy_loss = -0.06 * self.vel.y  # % of velocity is lost
@@ -36,26 +43,45 @@ class Node:
         force = -stiffness * extension + energy_loss
 
         self.vel.y += force * self.dt
+    
+    def apply_horizontal_physics(self):
+        # update movement
+        extension = self.rect.x - self.ori_pos[0]
+        energy_loss = -0.06 * self.vel.x  # % of velocity is lost
+
+        stiffness = 0.04 # stiffness of spring which determines its movment (less = bouncy, more = rigid)
+        force = -stiffness * extension + energy_loss
+
+        self.vel.x += force * self.dt
 
     def update_pos(self):
+        self.x += self.vel.x * self.dt
+        self.rect.centerx = self.x
         self.y += self.vel.y * self.dt
         self.rect.centery = self.y
 
-    def spread(self, node, dt, speed=0.06):
+    def spread_horizontal_neigbors(self, node, dt, speed=0.06):
         self.vel.y += (node.y - self.y) * speed * dt
+    
+    def spread_vertical_neigbors(self, node, dt, speed=0.06):
+        self.vel.x += (node.x - self.x) * speed * dt
 
 
 class NodeManager:
     def __init__(self):
-        self.size = 16
+        self.size = 10
 
-        self.nodes = [Node((x, 300), 10) for x in range(0, 600, 10)]
+        self.nodes = [Node((300, x), self.size) for x in range(0, 600+1, 10)]
 
     def draw(self, draw_surf, camera_offset=[0, 0]):
         # [node.draw(draw_surf, camera_offset) for node in self.nodes]
-        start_node = [(self.nodes[0].rect.x, 400)]
-        end_node = [(self.nodes[-1].rect.x, 400)]
-        pygame.draw.polygon(draw_surf, '#1e66e3', start_node + [node.rect.topleft for node in self.nodes] + end_node)
+        # start_node = [(self.nodes[0].rect.x, 420)] # horizontal
+        # end_node = [(self.nodes[-1].rect.x, 420)] # horizontal
+
+        start_node = [(600, self.nodes[0].rect.y)] # vertical
+        end_node = [(600, self.nodes[-1].rect.y)] # vertical
+
+        pygame.draw.polygon(draw_surf, 'blue', start_node + [node.rect.topleft for node in self.nodes] + end_node)
         pygame.draw.lines(draw_surf, 'white', True, start_node + [node.rect.topleft for node in self.nodes] + end_node, 4)
     
     def update(self, delta_time):
@@ -65,9 +91,12 @@ class NodeManager:
             if not node.interact(pygame.mouse.get_pos()):
                 node.update(self.dt)
                 node.update_pos()
-                node.wave(time.time())
+                # node.wave_vertical(time.time())
+                node.wave_horizontal(time.time())
 
             if i > 0:
-                self.nodes[i - 1].spread(node, self.dt, speed=0.1)
+                # self.nodes[i - 1].spread_horizontal_neigbors(node, self.dt, speed=0.1)
+                self.nodes[i - 1].spread_vertical_neigbors(node, self.dt, speed=0.1)
             if i < len(self.nodes) - 1:
-                self.nodes[i + 1].spread(node, self.dt, speed=0.1)
+                # self.nodes[i + 1].spread_horizontal_neigbors(node, self.dt, speed=0.1)
+                self.nodes[i + 1].spread_vertical_neigbors(node, self.dt, speed=0.1)
